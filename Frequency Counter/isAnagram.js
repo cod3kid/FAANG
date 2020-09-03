a54393b2f770c9d1c7e2dbbaa7d31e17fee65095
Muameddo Sufeeru <37668040+cod3kid@users.noreply.github.com>
2020-09-04 05:23:46 +0530

    Add files via upload

---
+++ Frequency Counter/isAnagram.js
@@ -0,0 +1,35 @@
+//Frequency Counter Method to find Anagram in linear time O(n)
+
+function isAnagram(str1, str2) {
+
+    if (str1.length != str2.length)
+        return false
+
+    lookup = {}
+
+    for (let i in str1) {
+
+        let letter = str1[i]
+        lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
+    }
+
+    for (let i in str2) {
+
+        let letter = str2[i]
+
+        if (!lookup[letter]) {
+            return false
+        }
+        else {
+            lookup[letter] -= 1
+        }
+    }
+
+    return true
+}
+
+
+str1 = "eat"
+str2 = "ate"
+
+console.log(isAnagram(str1, str2))
