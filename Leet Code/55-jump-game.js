d637815fd0342def14d42b95a4367ae7d2937e3c
Muameddo Sufeeru <37668040+cod3kid@users.noreply.github.com>
2020-10-13 22:28:39 +0530

    Medium

---
+++ Leet Code/55-jump-game.js
@@ -0,0 +1,19 @@
+//Leetcode 55 Medium
+//DP
+
+function jumpGame(arr) {
+    let n = arr.length
+    let can_reach = 0
+
+    for (let i = can_reach; i <= can_reach; i++) {
+        if (i == n - 1) {
+            return true
+        }
+
+        can_reach = Math.max(can_reach, i + arr[i])
+    }
+    return false
+}
+
+arr = [2, 3, 1, 1, 4]
+console.log(jumpGame(arr))
