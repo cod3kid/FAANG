e17e177c3f6433bc784e05c17db4c41f85055bdd
Muhamed Sufail <djsufail@gmail.com>
2020-10-28 23:54:47 +0530

    Medium

--- Leet Code/799-champagne-tower.js
+++ Leet Code/799-champagne-tower.js
@@ -1,23 +1,19 @@
 function champagneTower(poured, query_row, query_glass) {
-    tower = []
-    for (let i = 0; i < query_row; i++) {
-        tower.push([])
-    }
-    tower[0][0] = poured
-
-    for (let row = 0; row < query_row; row++) {
-        let maxCol = Math.min(row, query_glass)
-        for (let col = 0; col <= maxCol; col++) {
-            if (tower[row][col] > 1) {
-                double_spill = tower[row][col] - 1
-                tower[row + 1][col] += double_spill / 2
-                tower[row + 1][col + 1] += double_spill / 2
+    let flow_through = [];
+    flow_through[0] = [poured];
+    for (let row = 1; row <= query_row; row++) {
+        flow_through[row] = new Array(row + 1).fill(0);
+        for (let glass = 0; glass <= row; glass++) {
+            if (glass <= row - 1 && flow_through[row - 1][glass] > 1)
+                flow_through[row][glass] += (flow_through[row - 1][glass] - 1) / 2;
+            if (glass - 1 >= 0 && flow_through[row - 1][glass - 1] > 1) {
+                flow_through[row][glass] += (flow_through[row - 1][glass - 1] - 1) / 2;
             }
-        }
+
+        };
     }
 
-    result = tower[query_row][query_glass] > 1 ? 1.0 : tower[query_row][query_glass]
-    return result
+    return flow_through[query_row][query_glass] > 1 ? 1 : flow_through[query_row][query_glass];
 }
 
 poured = 1, query_row = 10, query_glass = 1
