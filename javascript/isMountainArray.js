c58b9370dcecd6be2c791e343bab2a595f644a6c
cod3kid <djsufail@gmail.com>
2022-03-21 22:57:09 +0530

    Mountain Array Completed

--- javascript/isMountainArray.js
+++ javascript/isMountainArray.js
@@ -1,27 +1,25 @@
-function isMountainArray(num) {
+function isMountainArray(arr) {
   isPeakFound = false;
-  numLen = num.length;
+  if (arr.length < 3) return false;
 
-  if (numLen < 3) {
-    return false;
+  for (i = 0; i < arr.length; i++) {
+    // For leveling [3,5,5,1,1]
+    if (arr[i] == arr[i + 1]) return false;
+
+    // For increasing after finding peak
+    if (isPeakFound && arr[i] < arr[i + 1]) return false;
+
+    // For finding peak
+    if (arr[i] > arr[i + 1]) isPeakFound = true;
+
+    // For decreasing from the start [9,8,7,6,5,4,3,2,1,0]
+    if (i == 0 && isPeakFound) return false;
+
+    // For increasing from the start [0,1,2,3,4,5,6,7,8,9]
+    if (i + 1 == arr.length && !isPeakFound) return false;
   }
-
-  for (i = 1; i < num.length; i++) {
-    if (num[i] == num[i - 1]) return false;
-    else if (num[i] < num[i - 1]) isPeakFound = true;
-    else if (isPeakFound && num[i] > num[i - 1]) {
-      return false;
-    }
-
-    if (num[numLen - 1] > num[numLen - 2]) {
-      return false;
-    }
-
-    if (i == num.length && !isPeakFound) return false;
-  }
-
   return true;
 }
 
-num = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
-console.log(isMountainArray(num));
+arr = [0, 1, 2, 4, 2, 1];
+console.log(isMountainArray(arr));
