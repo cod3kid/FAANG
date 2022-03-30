5367962176e070f0026dd335f068768371379cc2
cod3kid <djsufail@gmail.com>
2022-03-30 23:47:59 +0530

    LeetCode 4 : Part 1

---
+++ python/LeetCode/4-median-of-two-sorted-arrays.py
@@ -0,0 +1,15 @@
+def median(arr1,arr2):
+  
+ arr3 = arr1+arr2
+ arr3= arr3.sort()
+ print(arr3)
+ array_length = len(arr3)
+
+ if array_length %2==0:
+     return arr3[array_length//2]+arr3[(array_length//2)-1]
+ else:
+     return arr3[array_length//2]
+   
+arr1 = [1,2]
+arr2 = [3,4,5]
+print(median(arr1,arr2))
