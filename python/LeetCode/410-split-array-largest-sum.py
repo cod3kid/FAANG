3333da315a9965cb2ca384ad6a0b2f5d0b9837ed
cod3kid <djsufail@gmail.com>
2022-04-03 18:31:47 +0530

    LeetCode 410 => Part 4

--- python/LeetCode/410-split-array-largest-sum.py
+++ python/LeetCode/410-split-array-largest-sum.py
@@ -4,6 +4,16 @@ def splitArrayLargestSum(arr):
 
    while left <= right:
        mid = left + ((right -1)//2)
+       if canSplit(mid):
+           result = mid
+           right = mid - 1
+       else:
+           left = mid+1
+
+
+   def canSplit(largest):
+        subArray=0
+        curSum = 0
 
 
 arr = [7,2,5,10,18]
