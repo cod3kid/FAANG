b4fe8f9cecc62ebfe84774ec949a99913a7f72aa
cod3kid <djsufail@gmail.com>
2022-04-08 20:00:00 +0530

    AlgoExpert Two Number Sum  => HashMap Approach

---
+++ python/AlgoExpert/two-number-sum.py
@@ -0,0 +1,23 @@
+def twoNumberSum(arr, targetSum):
+    # Time Complexity O(n^2)
+    # for i in range(len(arr)):
+    #     for j in range(len(arr)):
+    #         if i!=j and arr[i]+arr[j] == targetSum :
+    #             return [arr[i],arr[j]]
+
+    # Time Complexity O(n)
+    nums = {}
+    for i in range(len(arr)):
+        potentialMatch  = targetSum - arr[i] 
+        if potentialMatch in nums:
+            return [potentialMatch,arr[i]]
+        else:
+            nums[arr[i]]=True
+
+    return []
+
+
+arr = [3, 5, -4, 8, 11, 1, -1, 6]
+targetSum = 10
+print(twoNumberSum(arr,targetSum))
+    
