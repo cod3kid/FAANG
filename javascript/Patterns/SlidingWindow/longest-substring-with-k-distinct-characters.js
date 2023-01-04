dab19ae808c6d293fe7a9b78b0d752a59173ea3c
cod3kid <djsufail@gmail.com>
2023-01-04 17:17:24 +0530

    Sliding Window: Longest Substring with K Distinct Characters

---
+++ javascript/Patterns/SlidingWindow/longest-substring-with-k-distinct-characters.js
@@ -0,0 +1,33 @@
+function longestSubstringWithKDistinctChars(string, k) {
+  if (string == null || string.length == 0 || k == 0) return 0;
+
+  start = 0;
+  windowString = "";
+  lookUp = {};
+  maxLen = 0;
+
+  for (i = 0; i < string.length; i++) {
+    windowString = string.slice(start, i + 1);
+
+    if (lookUp[string[i]]) {
+      lookUp[string[i]] += 1;
+    } else {
+      lookUp[string[i]] = 1;
+    }
+
+    while (Object.keys(lookUp).length > k) {
+      lookUp[string[start]] -= 1;
+      if (lookUp[string[start]] == 0) {
+        delete lookUp[string[start]];
+      }
+      start++;
+    }
+    maxLen = Math.max(maxLen, i - start + 1);
+  }
+
+  return maxLen;
+}
+
+string = "cbbebi";
+k = 3;
+console.log(longestSubstringWithKDistinctChars(string, k));
