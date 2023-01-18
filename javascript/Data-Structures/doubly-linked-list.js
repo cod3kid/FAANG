9a14df0ba44f5d4bf60c2987dcb2a2d80335d209
cod3kid <djsufail@gmail.com>
2023-01-18 18:29:54 +0530

    DS: Doubly Linked List: Push method

---
+++ javascript/Data-Structures/doubly-linked-list.js
@@ -0,0 +1,29 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.prev = null;
+    this.next = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(value) {
+    let newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+
+    this.length++;
+  }
+}
