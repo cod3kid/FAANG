26c5b9bebb69636157eb0fa6060b1c5647929f9b
cod3kid <djsufail@gmail.com>
2023-02-02 00:57:55 +0530

    Sliding Window: Maximum Sum SubArray of Size K: Approach 2

--- javascript/Patterns/SlidingWindow/maximum-sum-subarray-of-size-k.js
+++ javascript/Patterns/SlidingWindow/maximum-sum-subarray-of-size-k.js
@@ -16,6 +16,21 @@ function maximumSumSubArrayOfSizeK(arr, k) {
   return max;
 }
 
+const maximumSumSubArrayOfSizeK = (arr, k) => {
+  let maxSum = 0;
+  let subArray = [];
+
+  for (let i = 0; i < arr.length - 2; i++) {
+    const currentSum = arr[i] + arr[i + 1] + arr[i + 2];
+    if (currentSum > maxSum) {
+      maxSum = currentSum;
+      subArray = [arr[i], arr[i + 1], arr[i + 2]];
+    }
+  }
+
+  return subArray;
+};
+
 arr = [2, 1, 5, 1, 3, 2];
 k = 3;
 console.log(maximumSumSubArrayOfSizeK(arr, k));
