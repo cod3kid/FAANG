cefc4c74432aa15262cf10b284b49ca2ea2259b3
cod3kid <djsufail@gmail.com>
2023-02-08 17:42:20 +0530

    Data Structures: Graph: addEdge method

--- javascript/Data-Structures/graph.js
+++ javascript/Data-Structures/graph.js
@@ -8,4 +8,21 @@ class Graph {
       this.adjacencyList[vertex] = new Set();
     }
   }
+
+  addEdge(vertex1, vertex2) {
+    if (!this.adjacencyList[vertex1]) {
+      this.addVertex(vertex1);
+    }
+
+    if (!this.adjacencyList[vertex2]) {
+      this.addVertex(vertex2);
+    }
+
+    this.adjacencyList[vertex1].add(vertex2);
+    this.adjacencyList[vertex2].add(vertex1);
+  }
 }
+
+const graph = new Graph();
+graph.addEdge("A", "B");
+console.log(graph.adjacencyList);
