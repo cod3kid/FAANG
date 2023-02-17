8de5e7ef0d52580eeceda6052d31d8a0f725bab4
cod3kid <djsufail@gmail.com>
2023-02-17 08:14:25 +0530

    Recursion: Nth Fibonacci: Memoize method linear time

--- javascript/Patterns/Recursion/nth-fibonacci.js
+++ javascript/Patterns/Recursion/nth-fibonacci.js
@@ -1,9 +1,18 @@
 // O(2^n) time |  O(n) space
-function getNthFib(n) {
-  if (n === 1 || n === 2) return n - 1;
+// function getNthFib(n) {
+//   if (n === 1 || n === 2) return n - 1;
 
-  return getNthFib(n - 1) + getNthFib(n - 2);
+//   return getNthFib(n - 1) + getNthFib(n - 2);
+// }
+
+// O(n) time | O(n) space
+function getNthFib(n, memoize = { 1: 0, 2: 1 }) {
+  // Check whether the key exist
+  if (n in memoize) {
+    return memoize[n];
+  }
+  memoize[n] = getNthFib(n - 1, memoize) + getNthFib(n - 2, memoize);
+  return memoize[n];
 }
-
 console.log(getNthFib(7));
 // 0 1 1 2 3 5 8
