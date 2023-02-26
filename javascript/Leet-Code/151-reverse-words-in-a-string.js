f28072316eda9f4fb5f2d67ddff7beeeaf5bebb0
cod3kid <djsufail@gmail.com>
2023-02-26 21:45:17 +0530

    LeetCode: 151: Reverse words in a string: Approach 2

--- javascript/Leet-Code/151-reverse-words-in-a-string.js
+++ javascript/Leet-Code/151-reverse-words-in-a-string.js
@@ -1,10 +1,23 @@
-const reverseWords = function (s) {
-  return s
-    .trim()
-    .split(" ")
-    .filter((item) => item.length)
-    .reverse()
-    .join(" ");
+// const reverseWords = function (s) {
+//   return s
+//     .trim()
+//     .split(" ")
+//     .filter((item) => item.length)
+//     .reverse()
+//     .join(" ");
+// };
+
+const reverseWords = (s) => {
+  let words = s.split(" ");
+  let output = "";
+
+  for (let i = words.length - 1; i >= 0; i--) {
+    if (words[i].length === 0) continue;
+
+    output += " " + words[i];
+  }
+
+  return output.slice(1);
 };
 
 let str = "a good   example";
