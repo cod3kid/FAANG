e24a472519e6f2a221dcd9bb0e2cc945afb9d75f
cod3kid <djsufail@gmail.com>
2023-02-28 00:15:16 +0530

    LeetCode: 238: Product of Array Except Itself: Prefix n Suffix method  O(n)

--- javascript/Leet-Code/238-product-of-array-except-itself.js
+++ javascript/Leet-Code/238-product-of-array-except-itself.js
@@ -1,17 +1,37 @@
+// const productExceptSelf = function (nums) {
+//   let products = [];
+//   for (let i = 0; i < nums.length; i++) {
+//     let currentProduct = 1;
+//     for (j = 0; j < nums.length; j++) {
+//       if (j !== i) {
+//         currentProduct *= nums[j];
+//       }
+//     }
+//     products.push(currentProduct);
+//   }
+
+//   return products;
+// };
+
+// Prefix and Suffix Approach | Time O(n)
+
 const productExceptSelf = function (nums) {
-  let products = [];
+  let products = Array(nums.length).fill(1);
+
+  let prefix = 1;
   for (let i = 0; i < nums.length; i++) {
-    let currentProduct = 1;
-    for (j = 0; j < nums.length; j++) {
-      if (j !== i) {
-        currentProduct *= nums[j];
-      }
-    }
-    products.push(currentProduct);
+    products[i] = prefix;
+    prefix *= nums[i];
+  }
+
+  let suffix = 1;
+  for (let i = nums.length - 1; i >= 0; i--) {
+    products[i] *= suffix;
+    suffix *= nums[i];
   }
 
   return products;
 };
 
-let arr = [1, 2, 3, 4];
+let arr = [5, 1, 4, 2];
 console.log(productExceptSelf(arr));
