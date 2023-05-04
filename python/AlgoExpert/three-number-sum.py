a546a13c28943785c204cc59ae2a16e4ca32ccbd
cod3kid <djsufail@gmail.com>
2023-05-04 22:45:25 +0530

    Three Number Sum: Optimal Solution : Done=> AlgoExpert

--- python/AlgoExpert/three-number-sum.py
+++ python/AlgoExpert/three-number-sum.py
@@ -1,18 +1,26 @@
-# def nonConstructibleChange(arr):
-#     sortedArr = sorted(arr)
-#     print(sorted(arr))
-#     currentChangeCreated = 0
-#     for i in sortedArr:
-#         if i > currentChangeCreated+1:
-#           return currentChangeCreated + 1
-        
-#         currentChangeCreated+=i
-
-#     return currentChangeCreated+1
-
-
-
 def threeNumberSum(arr, targetSum):
+  sortedArr = sorted(arr)
+  result = []
+  for i in range(len(sortedArr)-2):  
+     
+     leftPtr = i + 1
+     rightPtr = len(sortedArr)-1
+
+     while leftPtr < rightPtr:
+       threeSums = sortedArr[i] + sortedArr[leftPtr] + sortedArr[rightPtr]
+       if threeSums == targetSum:
+         result.append([sortedArr[i],sortedArr[leftPtr],sortedArr[rightPtr]])
+         leftPtr +=1
+         rightPtr-=1
+        
+       if threeSums > targetSum:
+         rightPtr-=1
+       if threeSums < targetSum:
+         leftPtr+=1
+
+  return result
+
+def threeNumberSumBruteForce(arr, targetSum):
   sortedArr = sorted(arr)
 
   i=0
@@ -37,7 +45,7 @@ def threeNumberSum(arr, targetSum):
 
 arr = [12, 3, 1, 2, -6, 5, -8, 6]
 targetSum = 0
-# print(nonConstructibleChange(arr))
+# print(threeNumberSumBruteForce(arr, targetSum))
 print(threeNumberSum(arr, targetSum))
 
 
