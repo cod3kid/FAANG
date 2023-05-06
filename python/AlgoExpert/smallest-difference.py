eb3adaa17ba197af5bbf357e5325274d16dc85c2
cod3kid <djsufail@gmail.com>
2023-05-06 23:42:15 +0530

    Smallest Difference: Brute Force: Fully Done => AlgoExpert

--- python/AlgoExpert/smallest-difference.py
+++ python/AlgoExpert/smallest-difference.py
@@ -1,28 +1,19 @@
 # def threeNumberSum(arr, targetSum):
 
-
 def smallestDifferenceBruteForce(arr1, arr2):
-
     sortedArr1 = sorted(arr1)
     sortedArr2 = sorted(arr2)
 
     smallestDiff = sortedArr1[len(sortedArr1)-1] + sortedArr2[len(sortedArr2)-1]
     result = [sortedArr1[len(sortedArr1)-1],arr2[len(sortedArr2)-1]]
 
-    print(len(sortedArr1))
 
     for i in range(len(sortedArr1)):
         for j in range(len(sortedArr2)):
-            currentDiff = 0
-            if (sortedArr1[i] < 0 and sortedArr2[j] > 0) or (sortedArr1[i] > 0 and sortedArr2[j]>0):
-                currentDiff = abs(sortedArr1[i] - sortedArr2[j])
-            if (sortedArr1[i] < 0 and sortedArr2[j] < 0) or (sortedArr1[i] > 0 and sortedArr2[j] < 0):
-                currentDiff = abs(sortedArr1[i] + sortedArr2[j])
-
+            currentDiff = abs(sortedArr1[i] - sortedArr2[j])
             if currentDiff < smallestDiff:
-                print(abs(sortedArr1[i]),abs(sortedArr2[j]), currentDiff)
                 smallestDiff = currentDiff
-                result = [abs(sortedArr1[i]), abs(sortedArr2[j])]
+                result = [sortedArr1[i], sortedArr2[j]]
 
     return result
 
