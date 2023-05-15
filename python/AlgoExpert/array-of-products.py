1b7c64e326fba0d374182ef12373aad2d18948ae
cod3kid <djsufail@gmail.com>
2023-05-15 21:38:46 +0530

    AlgoExpert: Array of Products: Optimal Solution

--- python/AlgoExpert/array-of-products.py
+++ python/AlgoExpert/array-of-products.py
@@ -1,4 +1,5 @@
-def arrayOfProducts(array):
+# O(n^2)
+def arrayOfProductsNaive(array):
     
     output = []
     for i in range(len(array)):
@@ -9,7 +10,26 @@ def arrayOfProducts(array):
         output.append(currProduct)
     return output
 
+# O(n)
+def arrayOfProducts(array):
+    leftOutputArr = [1 for _ in range(len(array))]
+    rightOutputArr = [1 for _ in range(len(array))]
+    output = [1 for _ in range(len(array))]
+   
+    product = 1
+    for i in range(len(array)):
+       leftOutputArr[i] = product
+       product *= array[i]
 
+    product = 1
+    for i in reversed(range(len(array))):
+       rightOutputArr[i] = product
+       product *= array[i] 
+
+    for i in range(len(array)):
+       output[i] = leftOutputArr[i] * rightOutputArr[i]
+
+    return output
 
 
 arr = [5, 1, 4, 2]
