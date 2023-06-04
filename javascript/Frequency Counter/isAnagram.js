04a09a2e61b97b29afbbc63d51d278284580d2f7
cod3kid <djsufail@gmail.com>
2023-06-04 23:13:55 +0530

    Frequency Counter: isAnagram: My Solution O(nlogn)

--- javascript/Frequency Counter/isAnagram.js
+++ javascript/Frequency Counter/isAnagram.js
@@ -1,35 +1,30 @@
 //Frequency Counter Method to find Anagram in linear time O(n)
+// Alternate solution O(nlogn) sort the strings
 
 function isAnagram(str1, str2) {
+  if (str1.length != str2.length) return false;
 
-    if (str1.length != str2.length)
-        return false
+  lookup = {};
 
-    lookup = {}
+  for (let i in str1) {
+    let letter = str1[i];
+    lookup[letter] ? (lookup[letter] += 1) : (lookup[letter] = 1);
+  }
 
-    for (let i in str1) {
+  for (let i in str2) {
+    let letter = str2[i];
 
-        let letter = str1[i]
-        lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
+    if (!lookup[letter]) {
+      return false;
+    } else {
+      lookup[letter] -= 1;
     }
+  }
 
-    for (let i in str2) {
-
-        let letter = str2[i]
-
-        if (!lookup[letter]) {
-            return false
-        }
-        else {
-            lookup[letter] -= 1
-        }
-    }
-
-    return true
+  return true;
 }
 
+str1 = "eat";
+str2 = "ate";
 
-str1 = "eat"
-str2 = "ate"
-
-console.log(isAnagram(str1, str2))
+console.log(isAnagram(str1, str2));
