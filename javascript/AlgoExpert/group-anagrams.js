2ec8fc255f817acd99285b543a287d4e00f4fed1
cod3kid <djsufail@gmail.com>
2023-10-18 14:21:23 +0530

    Group Anagrams: Best Solution

--- javascript/AlgoExpert/group-anagrams.js
+++ javascript/AlgoExpert/group-anagrams.js
@@ -1,25 +1,41 @@
+// const groupAnagrams = (words) => {
+//   const result = [];
+
+//   words.forEach((word, index) => {
+//     const sortedWord = word.split("").sort().join("");
+//     if (index === 0) {
+//       result.push([word]);
+//     } else {
+//       let isPairFound = false;
+//       result.forEach((item) => {
+//         if (!isPairFound && item[0].split("").sort().join("") === sortedWord) {
+//           isPairFound = true;
+//           item.push(word);
+//         }
+//       });
+
+//       if (!isPairFound) {
+//         result.push([word]);
+//       }
+//     }
+//   });
+//   return result;
+// };
+
+// time: O(w * n log n) => w: no. of words, n: length of the longest word
 const groupAnagrams = (words) => {
-  const result = [];
+  const anagrams = {};
 
-  words.forEach((word, index) => {
+  words.forEach((word) => {
     const sortedWord = word.split("").sort().join("");
-    if (index === 0) {
-      result.push([word]);
+    if (anagrams[sortedWord]) {
+      anagrams[sortedWord].push(word);
     } else {
-      let isPairFound = false;
-      result.forEach((item) => {
-        if (!isPairFound && item[0].split("").sort().join("") === sortedWord) {
-          isPairFound = true;
-          item.push(word);
-        }
-      });
-
-      if (!isPairFound) {
-        result.push([word]);
-      }
+      anagrams[sortedWord] = [word];
     }
   });
-  return result;
+
+  return Object.values(anagrams);
 };
 
 words = ["yo", "act", "flop", "tac", "foo", "cat", "oy", "olfp"];
