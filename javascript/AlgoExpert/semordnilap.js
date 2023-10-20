b41e14de15bcb9cdbd641fd72849b3c265437e8a
cod3kid <djsufail@gmail.com>
2023-10-20 15:26:25 +0530

    Semordnilap: Best Solution

--- javascript/AlgoExpert/semordnilap.js
+++ javascript/AlgoExpert/semordnilap.js
@@ -1,16 +1,31 @@
-const semordnilap = (words) => {
-  const palindromes = {};
+// const semordnilap = (words) => {
+//   const palindromes = {};
 
+//   words.forEach((word) => {
+//     const sortedWord = word.split("").sort().join("");
+//     if (palindromes[sortedWord]) {
+//       palindromes[sortedWord].push(word);
+//     } else {
+//       palindromes[sortedWord] = [word];
+//     }
+//   });
+
+//   return Object.values(palindromes).filter((arr) => arr.length === 2);
+// };
+
+const semordnilap = (words) => {
+  const wordSet = new Set(words);
+  const result = [];
   words.forEach((word) => {
-    const sortedWord = word.split("").sort().join("");
-    if (palindromes[sortedWord]) {
-      palindromes[sortedWord].push(word);
-    } else {
-      palindromes[sortedWord] = [word];
+    const reversedWord = word.split("").reverse().join("");
+    if (wordSet.has(reversedWord)) {
+      result.push([word, reversedWord]);
+      wordSet.delete(word);
+      wordSet.delete(reversedWord);
     }
   });
 
-  return Object.values(palindromes).filter((arr) => arr.length === 2);
+  return result;
 };
 
 words = ["diaper", "abc", "test", "cba", "repaid"];
