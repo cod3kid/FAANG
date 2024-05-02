f0c0cac8b84f59d52b92405b6f3c22105164cbee
cod3kid <djsufail@gmail.com>
2024-05-02 12:55:24 +0530

    JS: Patterns: MergeIntervals - merge intervals clean up

--- javascript/Patterns/MergeIntervals/merge-intervals.js
+++ javascript/Patterns/MergeIntervals/merge-intervals.js
@@ -7,29 +7,31 @@ function mergeIntervals(intervals) {
   intervals.forEach((interval, index) => {
     if (index === 0) return;
 
+    const previouslyAddedInterval = result.at(-1)[0];
+
     if (
-      interval[0] >= result.at(-1)[0] &&
-      interval[0] <= result.at(-1)[1] &&
-      interval[1] >= result.at(-1)[0] &&
-      interval[1] <= result.at(-1)[1]
+      interval[0] >= previouslyAddedInterval &&
+      interval[0] <= previouslyAddedInterval &&
+      interval[1] >= previouslyAddedInterval &&
+      interval[1] <= previouslyAddedInterval
     )
       return;
 
     if (
-      interval[0] >= result.at(-1)[0] &&
-      interval[0] <= result.at(-1)[1] &&
-      interval[1] >= result.at(-1)[1]
+      interval[0] >= previouslyAddedInterval &&
+      interval[0] <= previouslyAddedInterval &&
+      interval[1] >= previouslyAddedInterval
     ) {
-      result[result.length - 1] = [result[result.length - 1][0], interval[1]];
+      result[result.length - 1] = [previouslyAddedInterval[0], interval[1]];
       return;
     }
 
     if (
-      interval[0] <= result.at(-1)[0] &&
-      interval[1] >= result.at(-1)[0] &&
-      interval[1] <= result.at(-1)[1]
+      interval[0] <= previouslyAddedInterval &&
+      interval[1] >= previouslyAddedInterval &&
+      interval[1] <= previouslyAddedInterval
     ) {
-      result[result.length - 1] = [interval[0], result[result.length - 1][1]];
+      result[result.length - 1] = [interval[0], previouslyAddedInterval[1]];
       return;
     }
 
