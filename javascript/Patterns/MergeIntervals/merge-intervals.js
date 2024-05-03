a7c48e57242c3b31a8f15609e6c987c3e9b665cd
cod3kid <djsufail@gmail.com>
2024-05-03 17:00:36 +0530

    JS: Patterns: MergeIntervals - merge intervals optimized solution

--- javascript/Patterns/MergeIntervals/merge-intervals.js
+++ javascript/Patterns/MergeIntervals/merge-intervals.js
@@ -1,42 +1,66 @@
+// function mergeIntervals(intervals) {
+//   // Replace this placeholder return statement with your code
+//   let result = [];
+
+//   result.push(intervals[0]);
+
+//   intervals.forEach((interval, index) => {
+//     if (index === 0) return;
+
+//     const previouslyAddedInterval = result.at(-1)[0];
+
+//     if (
+//       interval[0] >= previouslyAddedInterval &&
+//       interval[0] <= previouslyAddedInterval &&
+//       interval[1] >= previouslyAddedInterval &&
+//       interval[1] <= previouslyAddedInterval
+//     )
+//       return;
+
+//     if (
+//       interval[0] >= previouslyAddedInterval &&
+//       interval[0] <= previouslyAddedInterval &&
+//       interval[1] >= previouslyAddedInterval
+//     ) {
+//       result[result.length - 1] = [previouslyAddedInterval[0], interval[1]];
+//       return;
+//     }
+
+//     if (
+//       interval[0] <= previouslyAddedInterval &&
+//       interval[1] >= previouslyAddedInterval &&
+//       interval[1] <= previouslyAddedInterval
+//     ) {
+//       result[result.length - 1] = [interval[0], previouslyAddedInterval[1]];
+//       return;
+//     }
+
+//     result.push([interval[0], interval[1]]);
+//   });
+//   return result;
+// }
+
 function mergeIntervals(intervals) {
-  // Replace this placeholder return statement with your code
+  if (!intervals.length) return;
+
   let result = [];
 
-  result.push(intervals[0]);
+  result.push([intervals[0][0], intervals[0][1]]);
 
-  intervals.forEach((interval, index) => {
-    if (index === 0) return;
+  let lastAddedInterval, curStart, curEnd, prevEnd;
+  for (let i = 1; i < intervals.length; i++) {
+    lastAddedInterval = result[result.length - 1];
 
-    const previouslyAddedInterval = result.at(-1)[0];
+    curStart = intervals[i][0];
+    curEnd = intervals[i][1];
+    prevEnd = lastAddedInterval[1];
 
-    if (
-      interval[0] >= previouslyAddedInterval &&
-      interval[0] <= previouslyAddedInterval &&
-      interval[1] >= previouslyAddedInterval &&
-      interval[1] <= previouslyAddedInterval
-    )
-      return;
-
-    if (
-      interval[0] >= previouslyAddedInterval &&
-      interval[0] <= previouslyAddedInterval &&
-      interval[1] >= previouslyAddedInterval
-    ) {
-      result[result.length - 1] = [previouslyAddedInterval[0], interval[1]];
-      return;
+    if (prevEnd >= curStart) {
+      result[result.length - 1][1] = Math.max(curEnd, prevEnd);
+    } else {
+      result.push([curStart, curEnd]);
     }
-
-    if (
-      interval[0] <= previouslyAddedInterval &&
-      interval[1] >= previouslyAddedInterval &&
-      interval[1] <= previouslyAddedInterval
-    ) {
-      result[result.length - 1] = [interval[0], previouslyAddedInterval[1]];
-      return;
-    }
-
-    result.push([interval[0], interval[1]]);
-  });
+  }
   return result;
 }
 
@@ -44,7 +68,6 @@ console.log(
   mergeIntervals([
     [1, 5],
     [3, 7],
-    [4, 6],
-    [15, 20],
+    [4, 8],
   ])
 );
