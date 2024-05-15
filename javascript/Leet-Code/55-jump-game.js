a3a655cb887ea19c172601d0c52bf9f454809028
cod3kid <djsufail@gmail.com>
2024-05-16 04:30:12 +0530

    Small change

--- javascript/Leet-Code/55-jump-game.js
+++ javascript/Leet-Code/55-jump-game.js
@@ -2,18 +2,21 @@
 //DP
 
 function jumpGame(arr) {
-    let n = arr.length
-    let can_reach = 0
+  let n = arr.length;
+  let can_reach = 0;
 
-    for (let i = can_reach; i <= can_reach; i++) {
-        if (i == n - 1) {
-            return true
-        }
-
-        can_reach = Math.max(can_reach, i + arr[i])
+  for (let i = can_reach; i <= can_reach; i++) {
+    if (i == n - 1) {
+      return true;
     }
-    return false
+
+    can_reach = Math.max(can_reach, i + arr[i]);
+  }
+  return false;
 }
 
-arr = [2, 3, 1, 1, 4]
-console.log(jumpGame(arr))
+arr = [2, 3, 1, 1, 4];
+console.log(jumpGame(arr));
+// TC: 0(n)  SC: O(1)
+// If there are no 0, we'll always be able to reach the end
+// If there are 0, and if we cross it, we'll reach the end
