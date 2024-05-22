3c8bc7c1d3cc364def8e2bad2d9a268574957dfb
cod3kid <djsufail@gmail.com>
2024-05-22 12:51:58 +0530

    Fast and Slow Pointers: Linked List Cycle

---
+++ javascript/Leet-Code/141-linked-list-cycle.js
@@ -0,0 +1,15 @@
+const hasCycle = (head) => {
+  let slow = head;
+  let fast = head;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+
+    if (slow === fast) return true;
+  }
+
+  return false;
+};
+
+// Fast and Slow Pointers
