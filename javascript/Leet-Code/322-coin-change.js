9e34baf751102409836fb4ce79b587da470131da
cod3kid <djsufail@gmail.com>
2024-05-26 16:37:36 +0530

    LeetCode 322 Coin Change Update

--- javascript/Leet-Code/322-coin-change.js
+++ javascript/Leet-Code/322-coin-change.js
@@ -2,26 +2,20 @@
 // Dynamic Programming
 
 function coinChange(coins, amount) {
+  let dp = Array(amount + 1).fill(amount + 1);
+  dp[0] = 0;
 
-    dp = []
-    for (let i = 0; i <= coins.length; i++) {
-        dp[i] = amount + 1
+  for (let i = 1; i <= amount; i++) {
+    for (let j = 0; j < coins.length; j++) {
+      if (i - coins[j] >= 0) {
+        dp[i] = Math.min(dp[i], 1 + dp[i - coins[j]]);
+      }
     }
-    console.log(dp)
+  }
 
-    dp[0] = 0
-
-    for (let i = 0; i <= amount; i++) {
-        for (let j = 0; j < coins.length; j++) {
-            if (coins[j] <= i) {
-                dp[i] = Math.min(dp[i], 1 + dp[i - coins[j]])
-            }
-        }
-    }
-    return dp[amount] > amount ? -1 : dp[amount]
+  return dp[amount] != amount + 1 ? dp[amount] : -1;
 }
 
-
-coins = [1, 2, 5]
-amount = 11
-console.log(coinChange(coins, amount))
+coins = [1, 2, 5];
+amount = 11;
+console.log(coinChange(coins, amount));
