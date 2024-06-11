dd79a969a8f3e421bd98e7d7fe8658bffa0f7aed
cod3kid <djsufail@gmail.com>
2024-06-11 17:45:29 +0530

    Subsets: Leetcode 46: Permutations

--- javascript/Patterns/Subsets/permutations.js
+++ javascript/Patterns/Subsets/permutations.js
@@ -1,24 +1,3 @@
-var permArr = [],
-  usedChars = [];
-
-function permutations(input) {
-  var i, ch;
-  for (i = 0; i < input.length; i++) {
-    ch = input.splice(i, 1)[0];
-    usedChars.push(ch);
-    if (input.length == 0) {
-      permArr.push(usedChars.slice());
-    }
-    permutations(input);
-    input.splice(i, 0, ch);
-    usedChars.pop();
-  }
-  return permArr;
-}
-
-let input = [1, 4, 5];
-console.log(permutations(input));
-
 /**
  * @param {number[]} nums
  * @return {number[][]}
