e73abf30b6633ddca1815b7293af529883641bb7
cod3kid <djsufail@gmail.com>
2025-01-31 14:32:57 +0530

    Leetcode 384 - Arrays - Shuffle an Array

---
+++ javascript/Leet-Code/384-shuffle-an-array.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ */
+var Solution = function (nums) {
+  this.original = nums;
+  this.nums = [...this.original];
+};
+
+/**
+ * @return {number[]}
+ */
+Solution.prototype.reset = function () {
+  this.nums = [...this.original];
+  return this.nums;
+};
+
+/**
+ * @return {number[]}
+ */
+Solution.prototype.shuffle = function () {
+  for (let i = this.nums.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [this.nums[i], this.nums[j]] = [this.nums[j], this.nums[i]];
+  }
+  return this.nums;
+};
+
+/*
+Uses Fischer-Yates Algorithm
+*/
