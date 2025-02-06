ebe980c4a80810f60fa81bc51eecb302b6019e88
cod3kid <djsufail@gmail.com>
2025-02-07 04:44:21 +0530

    Leetcode 65 - Bitwise Manipulation - Add Binary

---
+++ javascript/Leet-Code/65-add-binary.js
@@ -0,0 +1,15 @@
+var addBinary = function (a, b) {
+  let x = BigInt("0b" + a);
+  let y = BigInt("0b" + b);
+
+  // Perform the binary addition using bitwise operations
+  while (y !== 0n) {
+    let withoutCarry = x ^ y;
+    let carry = (x & y) << 1n;
+    x = withoutCarry;
+    y = carry;
+  }
+
+  // Convert the result back to binary and return it as a string
+  return x.toString(2);
+};
